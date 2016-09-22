3cdd17c5fa4b61544c8313ad852506351921f22f
dianecharish <diane@codemagnus.com>
2016-09-22 13:27:23 +0800

    Added message modal

--- app/components/dashboard/dashboard-ctrl.js
+++ app/components/dashboard/dashboard-ctrl.js
@@ -5,11 +5,21 @@
         .module('app')
         .controller('DashboardCtrl', DashboardCtrl);
 
-    DashboardCtrl.$inject = ['$scope', '$state'];
+    DashboardCtrl.$inject = ['$scope', '$state','ModalService'];
 
-    function DashboardCtrl ($scope, $state) {
+    function DashboardCtrl ($scope, $state, ModalService) {
         var vm = this;
         
         vm.titleHeader = 'Dashboard';
+        vm.message_modal = message_modal;
+
+        
+        function message_modal () {
+        	var content =  {
+        		header:'Message',
+        		message:'Hello World!'
+        	}
+        	ModalService.message_modal(content);
+        }
     }
 })();
