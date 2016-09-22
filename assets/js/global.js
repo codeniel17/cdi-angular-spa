6e7eb914dbdfbf949db13164257e494824f9da3d
dianecharish <diane@codemagnus.com>
2016-09-22 11:09:44 +0800

    Added message js

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,6 +5,77 @@
 
 var GLOBAL = {
     host: 'http:host',
-    socket: 'http:socket'
+    socket: 'http:socket',
+    host : function (ids) {
+		var query_string = '';
+		for (var key in ids) {
+			var _id = '';
+			if (ids[key]) {
+				_id = '/' + ids[key];
+			}
+			var new_key = '/' + key + _id;
+			var new_str = query_string + new_key;
+			query_string = new_str;
+		};
+		return host + query_string;
+	},
+	header : function (token) {
+		if (token) {
+			return {
+				'Access-Token' : token
+			};
+		} else {
+			return false;
+		}
+	},
+	transform : function (file) {
+		if (file)
+			return angular.identity;
+		else
+			return false;
+	},
+	user : function (cookies, state, stateName) {
+		var user = cookies.getObject('user');
+		if (user) {
+			return user; 
+		} else if (stateName == 'admin-login' || stateName == 'login') {
+			state.go(stateName);
+		} else {
+			state.go('login');
+		}
+	},
+	query : function (params) {
+		if (params) 
+			return params;
+		else
+			return false;
+	},
+	check_file : function (data, callback) {
+		var fd = new FormData();
+		if (data.photo || data.logo || data.license || data.police_clearance || 
+			data.nbi_clearance || data.vehicle_photo) {
+			for (var key in data) {
+				fd.append(key, data[key]);
+			};
+			callback(fd, true);
+		} else {
+			callback(data, false);
+		}
+	},
+	clean_object : function (object) {
+		for(var key in object) {
+			if (!object[key]) {
+				delete object[key];
+			}
+		};
+		return object;
+	},
+	clean_data : function (data) {
+		if (!data) {
+			return false;
+		} else {
+			return data;
+		}
+	},
 
 };
---
+++ assets/js/message.js
@@ -0,0 +1,15 @@
+
+
+ var MESSAGE = {
+	alertMsg 		 : 'Are you sure you want to ',
+	alertDecomission : 'You have decommissioned ',
+	addSuccessMsg 	 : 'Successfully added ',
+	errorMsg 		 : 'Something went wrong. Unable to ',
+	udSuccessMsg 	 : 'Successfully updated ',
+	asSuccessMsg 	 : ' has been assigned to ',
+	unSuccessMsg 	 : ' has been unassigned',
+	decommission 	 : 'decommission ',
+	delete			 : 'delete',
+	assign 			 : 'assign', 
+	unassign 		 : 'unassign ',
+}
