6ab8b18c67085263e8a957462e6fc26ca2d4017e
dianecharish <diane@codemagnus.com>
2017-06-15 10:34:25 +0800

    updated form modal

--- app/helpers/global.js
+++ app/helpers/global.js
@@ -8,21 +8,7 @@ var GLOBAL = {
     host : 'https://jsonplaceholder.typicode.com', 
     socket : 'http:socket',
     socketUrl: 'http://52.74.192.141:8083',
-    roadApiKey : 'roadApiKey',
-    pcc: '63',
-    map_default_position : [14.67512834600248,121.04331314563751], //Quezon city
-    display_page: [10,15,20,30,50,100],
-    page_max_size: 5,
-    date_format: 'MMM dd, yyyy',
-    time_format: 'HH:mm',
-    datetime_format : 'HH:mm MMM dd, yyyy',
-    calendar : ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-    appCache : {
-        clients:'', accounts:'', sorters:'', users:'', 
-        messengers:'', epod_status:'', epod_failure_reason:'', zones:'', 
-        documents:'', contact_persons:'', soa:'', soa_ssf:'', soa_ff:'', 
-        soa_d:'', bundles:'', assignments:''
-    },
+    appCache : { },
     managePcc : function (data, field) { 
 
         if (data)
--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,3 +1,12 @@
 var UTILS = {
-	logo:"assets/img/logo.png"
+	logo:"assets/img/logo.png",
+    roadApiKey : 'roadApiKey',
+    pcc: '63',
+    map_default_position : [14.67512834600248,121.04331314563751], //Quezon city
+    display_page: [10,15,20,30,50,100],
+    page_max_size: 5,
+    date_format: 'MMM dd, yyyy',
+    time_format: 'HH:mm',
+    datetime_format : 'HH:mm MMM dd, yyyy',
+    calendar : ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 }
